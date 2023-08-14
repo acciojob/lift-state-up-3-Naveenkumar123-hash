117041d9314960c5c3c1020c276be66ad2df2e3d
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-08-14 07:45:07 +0000

    corrected-one

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,12 +11,12 @@ const Button=({handlefn})=>{
         <div>
             <h2>Child Component</h2>
             <button onClick={()=>{
-                clicked('option1')
+                clicked('Option 1')
             }}>Option 1</button>
             <div>
                 <h2>Child Component</h2>
                 <button onClick={()=>{
-                    clicked('option2')
+                    clicked('Option 2')
                 }}>Option 2</button>
             </div>
         </div>
